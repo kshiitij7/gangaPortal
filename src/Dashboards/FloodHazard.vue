b0b103f8839be9a141100cb14ae27fef3be6a9cc
kshiitij7 <kshiitijsrivastava20@gmail.com>
2025-02-06 10:29:01 +0530

    Update FloodHazard.vue

--- src/Dashboards/FloodHazard.vue
+++ src/Dashboards/FloodHazard.vue
@@ -1,32 +1,52 @@
 <template>
-<div class="FloodHazard">
+  <div class="FloodHazard">
     <v-main>
-        <LeftSideBar />
-        <div style="height: 85vh;">
-            <MapComponent :center="[82.5644, 25.1441]" :zoom="10"  />
-        </div>
-        <RightSideBar />
+      <Globe3D style="height: 85vh;"/>
+      <LeftSideBar />
+      <div :class="{ showMap: showMap }" class="map-container"
+      >
+        <MapComponent :center="[82.0662, 26.2648]" :zoom="10" />
+      </div>
+      <RightSideBar />
     </v-main>
-</div>
+  </div>
 </template>
 
 <script>
 import MapComponent from '@/components/MapComponent.vue';
+import Globe3D from '@/Layouts/Globe3D.vue';
 import LeftSideBar from '@/Layouts/LeftSideBar.vue';
 import RightSideBar from '@/Layouts/RightSideBar.vue';
 
 export default {
-    name: 'FloodHazard',
-    components: {
-        MapComponent,
-        RightSideBar,
-        LeftSideBar,
-    },
-    data() {
-        return {
-           
-        }
-    },
-
+  name: 'FloodHazard',
+  components: {
+    MapComponent,
+    RightSideBar,
+    LeftSideBar,
+    Globe3D,
+  },
+  data() {
+    return {
+      showMap: false, // Initially hidden
+    };
+  },
+  mounted() {
+    setTimeout(() => {
+      this.showMap = true;
+       // Make the map visible
+    }, 2500);
+  },
 };
 </script>
+
+<style scoped>
+.map-container {
+  height: 85vh;
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+.showMap {
+  opacity: 1;
+}
+</style>
