57d4ff167f3dec9c4bcc847a92eef8d58747d213
kshiitij7 <kshiitijsrivastava20@gmail.com>
2025-02-06 10:29:38 +0530

    Update SolidWaste.vue

--- src/Dashboards/SolidWaste.vue
+++ src/Dashboards/SolidWaste.vue
@@ -1,34 +1,53 @@
 <template>
-<div class="SolidWaste">
-    <v-main>
+    <div class="SolidWaste">
+      <v-main>
+        <Globe3D style="height: 85vh;"/>
         <LeftSideBar />
-        <div style="height: 85vh;">
-            <MapComponent :center="[82.0662, 26.2648]" :zoom="10"  />
+        <div :class="{ showMap: showMap }" class="map-container"
+        >
+          <MapComponent :center="[82.0662, 26.2648]" :zoom="10" />
         </div>
         <RightSideBar />
-    </v-main>
-</div>
-</template>
-
-    
-    
-<script>
-import MapComponent from '@/components/MapComponent.vue';
-import LeftSideBar from '@/Layouts/LeftSideBar.vue';
-import RightSideBar from '@/Layouts/RightSideBar.vue';
-
-export default {
+      </v-main>
+    </div>
+  </template>
+  
+  <script>
+  import MapComponent from '@/components/MapComponent.vue';
+  import Globe3D from '@/Layouts/Globe3D.vue';
+  import LeftSideBar from '@/Layouts/LeftSideBar.vue';
+  import RightSideBar from '@/Layouts/RightSideBar.vue';
+  
+  export default {
     name: 'SolidWaste',
     components: {
-        MapComponent,
-        RightSideBar,
-        LeftSideBar,
+      MapComponent,
+      RightSideBar,
+      LeftSideBar,
+      Globe3D,
     },
     data() {
-        return {
-        
-        }
+      return {
+        showMap: false, // Initially hidden
+      };
     },
-
-};
-</script>
+    mounted() {
+      setTimeout(() => {
+        this.showMap = true;
+         // Make the map visible
+      }, 2500);
+    },
+  };
+  </script>
+  
+  <style scoped>
+  .map-container {
+    height: 85vh;
+    opacity: 0;
+    transition: opacity 0.5s ease-in-out;
+  }
+  .showMap {
+    opacity: 1;
+  }
+  </style>
+  
